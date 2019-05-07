7c12d9fed90fa694b11ee41579172f6e67f205f3
Leonardo Palacio <lpalaciom@hotmail.com>
2019-05-06 20:33:02 -0500

    Con rutas perdidas CSS

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -14,6 +14,9 @@ export class FooterComponent implements OnInit {
   
   constructor(private _noticias:NoticiasService) { 
     this.noticias = _noticias.obternerNoticias(); 
+    
+    // * En caso de crear una propiedad del objeto para las URLs se puede eliminar este "for"
+
     for (let index = 0; index < _noticias.obternerNoticias().length; index++) {
       const tituloUrl = _noticias.obternerNoticias()[index]['titulo'].replace(/ /g,"-",).slice(0,40);
       this.url[index] =tituloUrl
