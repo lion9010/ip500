9cf772026a7131d02605ed53537c499bd669746b
Leonardo Palacio <lpalaciom@hotmail.com>
2019-05-13 20:14:55 -0500

    miembros terminado falta traducir

--- src/app/ip500-members/ip500-members.component.ts
+++ src/app/ip500-members/ip500-members.component.ts
@@ -12,12 +12,21 @@ export class Ip500MembersComponent implements OnInit {
   titulo: string= 'Miembros';
 
   miembros: any [] = [];
+  url: string []= []
 
   constructor(
     private _miembros : NoticiasService
   ) { 
     this.miembros = this._miembros.obtenerMiembros();
     console.log(this.miembros)
+
+    // * En caso de crear una propiedad del objeto para las URLs se puede eliminar este "for"
+
+    for (let index = 0; index < this.miembros.length; index++) {
+      const tituloUrl = this.miembros[index]['name'].replace(/ /g,"-",).slice(0,40);
+      this.url[index] =tituloUrl
+    }
+
   }
 
   ngOnInit() {
